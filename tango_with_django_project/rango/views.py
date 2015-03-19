ebf51b11c459981efebb29883aea95e85f68b465
MindBullets <jbalacy@gmail.com>
2015-03-18 18:50:29 -0700

    Added a new About view
    Updated rango/urls.py to add about to urlpatterns. Also updated
    rango/views.py. Added links between index and about.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 from django.core.context_processors import request
 
 def index(request):
-    return HttpResponse("Rango says, \"I am a Golden God!\"")
+    return HttpResponse("Rango says, \"I am a Golden God!\"</br><a href='/rango/about/'>About</a>")
 # Create your views here.
+
+def about(request):
+    return HttpResponse("Rango says, \"Here is the About Page\"</br><a href='/rango/'>Main</a>")
